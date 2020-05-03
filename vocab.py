191ecc118c527fad74263c8dd8f677c45759b1d2
Jingjie Zhan <jzhan@roblox.com>
2020-05-02 22:51:53 -0700

    Minor fix for question a.

--- vocab.py
+++ vocab.py
@@ -121,7 +121,7 @@ class VocabEntry(object):
 
         @returns sents_var: tensor of (max_sentence_length, batch_size)
         """
-        # word_ids = self.words2indices(sents)
+        word_ids = self.words2indices(sents)
         sents_t = pad_sents(word_ids, self['<pad>'])
         # sents_t = pad_sents(sents, '<pad>')
         sents_var = torch.tensor(sents_t, dtype=torch.long, device=device)
